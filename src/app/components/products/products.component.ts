9b356ad52e6aed34140f250e7a5841826c7355e4
Alejandra Paz <pdinaalejandra@gmail.com>
2018-12-09 18:16:59 -0600

    eliminando un dato de la base de datos en FireBase

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -22,7 +22,7 @@ export class ProductsComponent implements OnInit {
   
 
   deleteProduct(event,product){
-  	console.log(product);
+  	this.productsService.deleteProduct(product);
   }
 
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore} from '@angular/fire/firestore';
+import { AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument} from '@angular/fire/firestore';
 import { Product } from '../models/products';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
     
-    productsCollection;
+    productsCollection:AngularFirestoreCollection<Product>;
 	products: Observable<Product[]>;
-	productsDoc;
+	productDoc:AngularFirestoreDocument<Product>;
 
   constructor(public db: AngularFirestore) { 
-   this.products= this.db.collection('products').valueChanges();
+   //this.products= this.db.collection('products').valueChanges();
+
+   this.productsCollection=this.db.collection('products');
+   this.products=this.productsCollection.snapshotChanges().pipe(map(actions
+    =>{
+    return actions.map(a =>{
+      const data= a.payload.doc.data() as Product;
+      data.id=a.payload.doc.id;
+      return data;
+    });
+   }));
   }
 
   getProducts(){
 
   	return this.products;
+  }
+
+  deleteProduct(product: Product){
+      this.productDoc=this.db.doc(`products/${product.id}`);
+      this.productDoc.delete();
   }
 }
